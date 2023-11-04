a806aeb2cf727dd85334eab3cd8203c392fc49ad
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2023-11-05 00:24:16 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class first{
+    public static void main(String args[])
+    { 
+
+        System.out.print("enter number");
+        Scanner s =new Scanner(System.in);
+        int a=s.nextInt();
+        int b = s.nextInt();
+        int sum = a+b;
+        System.out.print(a+b +"\n");
+        System.out.print("hello");
+    }
+} 
